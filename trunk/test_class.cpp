eb0df12e0dcb08c951333d07dfe17aeae876b57c
camellyx@gmail.com <camellyx@gmail.com@06fb7371-82c2-117e-2f1e-e2e6b27cc23d>
2011-10-27 00:48:47 +0000

    a working draft

--- trunk/test_class.cpp
+++ trunk/test_class.cpp
@@ -2,7 +2,22 @@
 
 int main() {
   uint_tavl tavl;
-  uint_tavl::iterator tavl_iter;
-  tavl[1] = 2;
-  printf("%d\n", tavl[1]);
+  uint_tavl::iterator tavl_iter = tavl.begin();
+  for (int i=2;i<10;i++) {
+    tavl[i] = 100-i;
+  }
+  tavl_iter = tavl.begin();
+  uint_tavl::iterator tmp;
+  tmp.tavl_table = tavl.tree;
+  tmp.tavl_node = NULL;
+  printf("%d\n", tavl_iter != tmp);
+  printf("%d\n", second(tavl_iter));
+  for (uint_tavl::iterator i=tavl.begin();
+                           i!=tavl.end();i++) {
+    printf("%d\n", second(i));
+  }
+  for (int i=0;i<12;i++) {
+    printf("%d %d\n", i, first(tavl.upper_bound(i)));
+  }
+  return 0;
 }
